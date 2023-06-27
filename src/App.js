43222f0247ef4b71728336c32767f41ebb8d083f
Lerigou <marciaeduardaravadelli@gmail.com>
2023-06-27 17:18:23 -0300

    Estrutura de pastas e arquivos

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Home from './Pages/Home/home';
+import Purpose from './Pages/Purpose/purpose';
+import Product from './Pages/Product/product';
+import Footer from './Pages/Footer/footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <Purpose />
+      <Product />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Purpose/purpose.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Purpose() {
+    return (
+        <h1>Proposta funciona</h1>
+    )
+}
