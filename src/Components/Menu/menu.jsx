f53cba76dc4c43e35b93e8e5aa4e6225d01254c8
blackkseaw <blackkseaw@gmail.com>
2023-07-02 16:32:38 -0300

    Menu pronto

--- src/Components/Menu/menu.jsx
+++ src/Components/Menu/menu.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import "./menu.scss";
+
 
 export default function Menu() {
     return (
-        <h1>Menu funciona</h1>
+        <div className="navbar">
+            <div className="nav">
+                <div className="nav-right">
+                    <div className="nav-item">
+                        <a href="">Frutologia</a>
+                    </div>
+                </div>
+                <div className="nav-left">
+                    <div className="nav-item">
+                        <a href="">Pesquisar</a>
+                    </div>
+                    <div className="nav-item">
+                        <a href="">Missão</a>
+                    </div>
+                    <div className="nav-item">
+                        <a href="">Sobre</a>
+                    </div>
+                    <div className="nav-item">
+                        <a href="">Contato</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
