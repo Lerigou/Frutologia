66e55672eb84651c7e7b5ca3fa57c3c40ede4096
blackkseaw <blackkseaw@gmail.com>
2023-07-06 14:38:38 -0300

    Sessão sobre o produto

--- src/Pages/Product/product.jsx
+++ src/Pages/Product/product.jsx
@@ -1,47 +1,52 @@
 import React from "react";
 import "./product.scss";
+import Search from "../../Assets/search.svg"
+import Table from "../../Assets/table.svg"
 
 export default function Product() {
     return (
-        <div className="purpose">
+        <div className="product">
 
-            <div className="purpose-container">
-                <div className="purpose-title">
-                    <h1>Missão</h1>
-                    <p>Mas por quê?</p>
+            <div className="product-container">
+                <div className="product-title">
+                    <h1>Sobre a ferramenta</h1>
+                    <p>Aprenda a usar!</p>
                 </div>
 
-                <div className="cards-purpose">
+                <div className="cards-product">
 
                     <div className="card-left">
-                        <div className="raspberry-container">
+                        <div className="yellow-container">
                             <div className="container-content">
                                 <div className="title">
-                                    <h2>Transformar as experiências anteriores das crianças</h2>
+                                    <h2>O que é?</h2>
                                 </div>
                                 <div className="paragraph">
-                                    <p>Queremos romper com a ideia de que as frutas são "ruins" ou sem graça,
-                                        incentivando as crianças a explorarem e apreciarem a diversidade de cores, sabores
-                                        e benefícios nutricionais que as frutas têm a oferecer.</p>
+                                    <p>É uma ferramenta divertida e educativa que fornece informações
+                                        completas sobre diferentes frutas. Basta digitar o nome de uma
+                                        fruta e você receberá todos os detalhes sobre ela.</p>
                                 </div>
+                            </div>
+                            <div className="yellow-img">
+                                <img src={Search} />
                             </div>
                         </div>
                     </div>
 
                     <div className="card-right">
-                        <div className="lemon-container">
-                            <div className="lemon-img">
-                            </div>
+                        <div className="pink-container">
                             <div className="container-content">
                                 <div className="title">
-                                    <h2>Mostrar que as frutas também são divertidas!</h2>
+                                    <h2>Informações</h2>
                                 </div>
                                 <div className="paragraph">
-                                    <p>Queremos mostrar que as frutas são verdadeiras 
-                                        estrelas da natureza, capazes de proporcionar 
-                                        energia, vitaminas e nutrientes essenciais para 
-                                        o crescimento e desenvolvimento saudável.</p>
+                                    <p>Após pesquisar a fruta, nossa ferramenta retornará as principais
+                                        características da dela, incluindo família, ordem, calorias entre
+                                        outras informações nutricionais. </p>
                                 </div>
+                            </div>
+                            <div className="pink-img">
+                                <img src={Table} />
                             </div>
                         </div>
                     </div>
