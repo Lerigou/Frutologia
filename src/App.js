07e5194d3c56b6335813dc351bf332d62dec287d
blackkseaw <blackkseaw@gmail.com>
2023-07-06 17:58:17 -0300

    Funcional porém não perfeito

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
-// import './App.css';
-import '../src/css/styles.scss';
-import Home from './Pages/Home/home';
-import Purpose from './Pages/Purpose/purpose';
-import Product from './Pages/Product/product';
-import Footer from './Pages/Footer/footer';
-import Faq from './Pages/Faq/faq';
-import Menu from './Components/Menu/menu';
+import React from "react";
+import Menu from "./Components/Menu/menu";
+import Home from "./Pages/Home/home";
+import Purpose from "./Pages/Purpose/purpose";
+import Product from "./Pages/Product/product";
+import Footer from "./Pages/Footer/footer";
+import Faq from "./Pages/Faq/faq";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <div className="App">
-      <Menu />
-      <Home />
-      <Purpose />
-      <Product />
-      <Faq />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Menu />
+        <div id="home">
+          <Home />
+        </div>
+        <div id="purpose">
+          <Purpose />
+        </div>
+        <div id="product">
+          <Product />
+        </div>
+        <div id="faq">
+          <Faq />
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
+
+
+// import React from "react";
+// import Menu from "./Components/Menu/menu";
+// import Home from "./Pages/Home/home";
+// import Purpose from "./Pages/Purpose/purpose";
+// import Product from "./Pages/Product/product";
+// import Footer from "./Pages/Footer/footer";
+// import Faq from "./Pages/Faq/faq";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// export default function App() {
+//   return (
+//     <Router>
+//       <div className="App">
+//         <Menu />
+//         <Routes>
+//           <Route path="/home" element={<Home />} />
+//           <Route path="/purpose" element={<Purpose />} />
+//           <Route path="/product" element={<Product />} />
+//           <Route path="/faq" element={<Faq />} />
+//         </Routes>
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// }
